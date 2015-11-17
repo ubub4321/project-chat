d9c3bdf2490a7e4dc4c6e571fb18a0c7f0068b52
Kim hansaem <ubub1234@naver.com>
2015-11-16 21:23:09 -0800

    server test

--- main.c
+++ main.c
@@ -1,53 +1,25 @@
 #include <stdio.h>
-#include <sys/time.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <netinet/in.h>
-
-//메시지 최대 길이
-#define MSGLEN 64
-//접속자 최대 명수
-#define MAXCONNECT 5
-
-static int userlist_fd[MAXCONNECT];
-static char userlist_nick[MAXCONNECT][MSGLEN];
-//메인과 오른쪽 윈도우 선언
-static *mwin, *rwin;
-
-void initwin()
+#include <sys/socket.h>
+#include <sys/select.h>
+#define MAX_CLIENT 10 //최대 입장자(클라이언트)수
+#define CHATDATA 1024
+#define INVALID_SOCK -1
+int list_c[MAX_CLIENT]; //채팅 참가중인 클라이언트에 대한 연결소켓 저장
+char escape[ ] = "Q";
+char greeting[ ] = "Welcome to my chatting room\n";
+char CODE200[ ] = "Sorry room is full\n";
+void main (int argc, char *argv[])
 {
-  //메인 윈도우, 채팅 로그 출력 부분 선언
-  mwin = newwin(20, 52, 2, 1);
-  //오른쪽 윈도우, 유저 리스트 출력 부분 선언
-  rwin = newwin(MAXCONNECT + 2, 15, 2, 54);
-  //글씨 굵게 시작
-  attron(A_BOLD);
-  //맨 위 메시지 출력
-  mvprintw(1,0,"%s","\t\t\tChating Log\t\t\t User List");
-  //맨 아래 메시지 출력
-  mvprintw(24,1,"Quit - Ctrl + C, MAXCONNECT - %d", MAXCONNECT);
-  //글시 굵게 종료
-  attroff(A_BOLD);
-  //화면 재출력
-  refresh();
-  //메인 윈도우의 스크롤 기능 키기
-  scrollok(mwin, true);
-  //커서 숨기기
-  noecho();
-
-  //메인 윈도우 테두리 선언
-  box(mwin,'|','-'); 
-  //오른쪽 윈도우 테두리 선언
-  box(rwin,'|','-'); 
-  //메인 윈도우 재출력
-  wrefresh(mwin);
-  //오른쪽 윈도우 재출력
-  wrefresh(rwin);
-}
-
-void main()
-{
-	 initscr();
-
-  	 initwin();
-	printf("dsadsada");
-}
+ int c_socket, s_socket;
+ struct sockaddr_in s_addr, c_addr;
+ int len;
+ int nfds = 0;
+ int i,j,n;
+ fd_set read_fds;
+ char chatData[CHATDATA];
+ int res;
+ }
