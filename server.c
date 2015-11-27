d943b234ca0198f3cc3b67068f5f90e404efdb90
WooMang <jwm9010@naver.com>
2015-11-27 05:34:51 -0800

    server

---
+++ server.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#define MAXLINE 511
+int main(int argc,char* argv[])
