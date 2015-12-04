c115e6565a8152bc73be4bd67ed296f45691e33b
WooMang <jwm9010@naver.com>
2015-12-04 09:59:30 -0800

    server coding comment

--- server.c
+++ server.c
@@ -28,7 +28,8 @@ int main(int argc,char* argv[]){
          exit(0);
      }
 
-    serv_sock = socket(PF_INET,SOCK_STREAM, IPPROTO_TCP); 
+    serv_sock = socket(PF_INET,SOCK_STREAM, IPPROTO_TCP);// serv_sock를 소켓 파일 서술자로 만든다.
+ 
      if(serv_sock == -1){
          perror("socket() error\n");
          exit(0);
@@ -43,11 +44,11 @@ int main(int argc,char* argv[]){
         setsockopt(serv_sock,SOL_SOCKET,SO_REUSEADDR,&option,sizeof(option));
 
     if(bind(serv_sock, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) == -1){
-         perror("bind() error\n"); 
+         perror("bind() error\n");  //bind(). 소켓 파일 서술자에게 ip, port 주소를 가진 구조체를 넣어준다. 
          exit(0);
      }
 
-    if(listen(serv_sock,1) == -1){ 
+    if(listen(serv_sock,1) == -1){ // 소켓 대기열 생성
         perror("listen() error\n");
          exit(0);
      }
@@ -65,7 +66,7 @@ int main(int argc,char* argv[]){
     }
 
     if((pid=fork())==-1){close(conn_sock);perror("fork() error");exit(0);}
-     else if (pid == 0){
+     else if (pid == 0){//자식프로세스이다.
 
         while(1){
              fgets(buf,sizeof(buf),stdin);
@@ -74,12 +75,12 @@ int main(int argc,char* argv[]){
              if((strncmp,"exit",4) == 0){
                  puts("Good Bye.");
                  exit(0);
-             } 
-        }     
+             } //자식프로세스는 stdin으로 사용자가 입력한 문자를 buf에 저장하여 소켓에다 write 시스템콜을 이용해
+        }     //client에게 문자열을 보내고, exit를 입력시 종료하게 한다.
          exit(0);
      }
 
-    else if(pid>0){ 
+    else if(pid>0){ //부모프로세스이다.
          while(1){
              if((nbytes = read(conn_sock,buf,MAXLINE)) <0){
                  perror("read() error\n");
@@ -88,8 +89,8 @@ int main(int argc,char* argv[]){
              printf("%s",buf);
              if(strncmp(buf,"exit",4) == 0)
                  exit(0);
-             }
-         }     
+             }//부모프로세스는 client가 소켓에 보낸 문자열을 read로 읽어 저장한다.
+         }     //역시 exit 가 날라왔을시에는 종료.
      return 0;
  }
 
